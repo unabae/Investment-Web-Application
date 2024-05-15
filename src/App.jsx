b9044b5c91cb756efb99a83f6bdd78ba6a220fe4
Laurence Unabia <laurenceunabia@gmail.com>
2024-05-15 19:09:30 +0800

    update header app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput";
 import Result from "./components/Result";
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import headerLogo from "/src/assets/investment-calculator-logo.png"
+import headerLogo from "../assets/investment-calculator-logo.png"
 
 export default function Header() {
   return (
-    <div id="header">
-      <img src= {headerLogo} alt="" />
+    <header id ="header">
+      <img src= {headerLogo} alt="Logo showing a money bag" />
       <h1>Investment Calculator</h1>
-    </div>
+    </header>
   );
 }
